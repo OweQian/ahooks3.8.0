b2d1d2706d98199a8cbf0b7734752c4cc8be62fe
qian wang <1510106069@qq.com>
2024-07-01 17:13:53 +0800

    feat: useMap

---
+++ src/hooks/useMap/index.ts
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import useMemoizedFn from "../useMemoizedFn";
+
+const useMap = <K, T>(initialValue?: Iterable<readonly [K, T]>) => {
+  // 初始化
+  const getInitValue = () => new Map(initialValue);
+
+  const [map, setMap] = useState<Map<K, T>>(getInitValue);
+
+  // 添加元素
+  const set = (key: K, entry: T) => {
+    setMap((prev) => {
+      const temp = new Map(prev);
+      temp.set(key, entry);
+      return temp;
+    });
+  };
+
+  // 生成一个新的 Map 对象
+  const setAll = (newMap: Iterable<readonly [K, T]>) => {
+    setMap(new Map(newMap));
+  };
+
+  // 移除元素
+  const remove = (key: K) => {
+    setMap((prev) => {
+      const temp = new Map(prev);
+      temp.delete(key);
+      return temp;
+    });
+  };
+
+  // 重置
+  const reset = () => setMap(getInitValue());
+
+  // 获取元素
+  const get = (key: K) => map.get(key);
+
+  return [
+    map,
+    {
+      set: useMemoizedFn(set),
+      setAll: useMemoizedFn(setAll),
+      remove: useMemoizedFn(remove),
+      reset: useMemoizedFn(reset),
+      get: useMemoizedFn(get),
+    },
+  ] as const;
+};
+
+export default useMap;
