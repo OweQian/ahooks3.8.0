fbcfb1fb98dcde692e3d06bd61fbb00fd190bdd9
qian wang <1510106069@qq.com>
2024-07-13 10:54:10 +0800

    feat: useWhyDidYouUpdate

---
+++ src/hooks/useWhyDidYouUpdate/index.ts
@@ -0,0 +1,33 @@
+import { useEffect, useRef } from "react";
+
+export type IProps = Record<string, any>;
+
+const useWhyDidYouUpdate = (componentName: string, props: IProps) => {
+  // 上一次的 props
+  const prevProps = useRef<IProps>({});
+
+  useEffect(() => {
+    if (prevProps.current) {
+      // 获取所有 key
+      const allKeys = Object.keys({ ...prevProps.current, ...props });
+      const changedProps: IProps = {};
+      allKeys.forEach((key) => {
+        // 哪些 key 进行了更新
+        if (!Object.is(prevProps.current[key], props[key])) {
+          changedProps[key] = {
+            from: prevProps.current[key],
+            to: props[key],
+          };
+        }
+      });
+      // 有 diff，控制台输出
+      if (Object.keys(changedProps).length) {
+        console.log("[why-did-you-update]", componentName, changedProps);
+      }
+    }
+    // 更新 prevProps
+    prevProps.current = props;
+  });
+};
+
+export default useWhyDidYouUpdate;
